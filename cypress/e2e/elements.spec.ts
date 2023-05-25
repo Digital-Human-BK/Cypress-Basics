6863121dbc980fc5c72ea23cfd1b6088dc1a95ec
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-05-25 12:19:23 +0300

    `ADD` Working with radio buttons and checkboxes

---
+++ cypress/e2e/elements.spec.ts
@@ -0,0 +1,31 @@
+/// <reference types="cypress" />
+
+describe("Second suite", () => {
+  it("Radio buttons", () => {
+    cy.visit("/");
+    cy.contains("Forms").click();
+    cy.contains("Form Layouts").click();
+
+    cy.contains("nb-card", "Using the Grid")
+      .find('[type="radio"]')
+      .then((radioButtons) => {
+        cy.wrap(radioButtons)
+          .first()
+          .check({ force: true })
+          .should("be.checked");
+
+        cy.wrap(radioButtons).eq(1).check({ force: true });
+        cy.wrap(radioButtons).first().should("not.be.checked");
+        cy.wrap(radioButtons).eq(2).should("be.disabled");
+      });
+  });
+
+  it.only("Checkboxes", () => {
+    cy.visit("/");
+    cy.contains("Modal & Overlays").click();
+    cy.contains("Toastr").click();
+
+    cy.get('[type="checkbox"]').check({ force: true });
+    cy.get('[type="checkbox"]').eq(0).uncheck({ force: true });
+  });
+});
