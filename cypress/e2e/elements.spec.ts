ed1c8f53a1ec4aaa1c209615db3d4deb5cdc538f
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-05-25 20:28:20 +0300

    `ADD` Working with default window:confirm/alert

--- cypress/e2e/elements.spec.ts
+++ cypress/e2e/elements.spec.ts
@@ -163,12 +163,41 @@ describe("Elements suite", () => {
     });
   });
 
-  it.only("Tooltips and popups", () => {
+  it("Tooltips and popups", () => {
     cy.visit("/");
     cy.contains("Modal & Overlays").click();
     cy.contains("Tooltip").click();
 
     cy.contains("nb-card", "Colored Tooltips").contains("Default").click();
     cy.get("nb-tooltip").should("contain", "This is a tooltip");
+  });
+
+  it.only("window: alert/confirm", () => {
+    cy.visit("/");
+    cy.contains("Tables & Data").click();
+    cy.contains("Smart Table").click();
+
+    //1 this is with auto confirm
+    // cy.get("tbody tr").first().find(".nb-trash").click();
+    // cy.on("window:confirm", (confirm) => {
+    //   expect(confirm).to.equal("Are you sure you want to delete?");
+    // });
+
+    //2
+    const stub = cy.stub();
+    cy.on("window:confirm", stub);
+    cy.get("tbody tr")
+      .first()
+      .find(".nb-trash")
+      .click()
+      .then(() => {
+        expect(stub.getCall(0)).to.be.calledWith(
+          "Are you sure you want to delete?"
+        );
+      });
+
+    //3 this way we disable the auto confirm in the window:alert/confirm dialogs
+    // cy.get("tbody tr").first().find(".nb-trash").click();
+    // cy.on("window:confirm", () => false);
   });
 });
